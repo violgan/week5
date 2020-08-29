3aed4dd8fd28b3f9093a8b7cc033d2d4ed9fe290
Violetta Ganevich <vviolgan@gmail.com>
2020-08-29 10:22:05 -0400

    update

--- functions.py
+++ functions.py
@@ -1,9 +1,24 @@
 #Functions
-
+comp = 'Level up'
 def greet_user():
     print("Welcome to class!")
+    print("this was basic function")
 
-def greet_user_1():
-    print("Welcome to Level Up!")
+def greet_user_1(name):
+    print(f"Welcome to {name}!")
 
 greet_user()
+greet_user_1("Jungle")
+greet_user_1(comp)
+
+def greet_user_2(company, username):
+    print(f"{username}, Welcome to {company}!")
+    print("Have a Great Week!")
+
+
+greet_user_2("Chase", "Violetta")
+
+
+
+
+
